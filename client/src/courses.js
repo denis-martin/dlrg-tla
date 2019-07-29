5dc3441cff2438450c24a3a5d28cb605b912a42c
denis <github@misc-net.de>
2019-07-29 23:17:02 +0200

    fixed date type error

--- client/src/courses.js
+++ client/src/courses.js
@@ -190,8 +190,8 @@ DlrgTlaApp.controller('DlgCourseEditCtrl', function ($scope, $uibModalInstance, 
 		var sobj = DlrgTla.sId ? DlrgTla.getEntry("seasons", DlrgTla.sId) : null;
 		$scope.obj = { 
 			sId: DlrgTla.sId, ctId: -1, name: null, 
-			begin: (sobj && sobj.begin) ? sobj.begin : (new Date()).toISOString().split("T")[0], 
-			end: (sobj && sobj.end) ? sobj.end : (new Date()).toISOString().split("T")[0],
+			begin: (sobj && sobj.begin) ? sobj.begin.toISOString().split("T")[0] : (new Date()).toISOString().split("T")[0], 
+			end: (sobj && sobj.end) ? sobj.end.toISOString().split("T")[0] : (new Date()).toISOString().split("T")[0],
 			seasonPass: 0,
 		};
 		DlrgTla.resetData($scope.obj, "courses");
